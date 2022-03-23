efc1b3fd73c034aec7f52fac729db97bfb443ed1
omiras <oscar.miras@gmail.com>
2022-03-23 14:12:27 +0100

    iteration 2

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,50 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health; // todos los soliders tienen una propiedad que se llama 'health'
+    this.strength = strength;
+  }
+
+  attack() {
+    return this.strength;
+  }
+
+  // receiveDamage(7). Tengo que restar 7 puntos a mi vida
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    super(health, strength); // inicializar la clase padre
+    this.name = name; // los Vikingos además tienen 'name'
+  }
+
+  receiveDamage(damage) {
+    super.receiveDamage(damage);
+
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    }
+
+    return `${this.name} has died in act of combat`;
+
+  }
+
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 
 // Saxon
-class Saxon {}
+class Saxon { }
 
 // War
-class War {}
+class War { }
 
 
 
